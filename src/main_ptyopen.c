e0914035da92b2555b9b8f62b4835dc6e5294d51
Swan Rocher <swan.rocher@gmail.com>
2012-12-26 09:48:55 +0100

    add log file

--- src/main_ptyopen.c
+++ src/main_ptyopen.c
@@ -1,15 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/select.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include "interceptor.h"
 
 int main(int argc, char** argv) {
-	int fd_master;
+	int fd_master,fd_log;
 	const char* cmd = "/usr/games/nethack";
 	char buffer[2];
 	buffer[0] = ' ';
 	buffer[1] = '\0';
 	char c;
+	int running = 1;
 
 	if (ptyopen(cmd,&fd_master) < 0) {
 		perror("ptyopen");
@@ -21,7 +25,16 @@ int main(int argc, char** argv) {
 		return -2;      // TODO clean
 	}
 
-	while (1) {
+	// TODO think that if stdin first char is #
+	// the command should be buffered
+
+	fd_log = open("./ori.log",O_WRONLY|O_CREAT|O_TRUNC);
+	if (fd_log < 0) {
+		perror("log open");
+		return -3;      // TODO clean
+	}
+
+	while (running) {
 		fd_set fds;
 		FD_ZERO(&fds);
 		FD_SET(fd_master,&fds);
@@ -32,36 +45,47 @@ int main(int argc, char** argv) {
 		}
 
 		if (FD_ISSET(fd_master,&fds)) {
-			if (read(fd_master,buffer,sizeof(char)) < 0) {
+			if (read(fd_master,&c,sizeof(char)) < 0) {
 				perror("read");
+				running = 0;
 				break;
 			}
-			printf("%c\n",buffer[0]);
+			printf("%c\n",c);
+			if (write(fd_log,&c,sizeof(char)) < 0) {
+				perror("log write");
+				running = 0;
+				break;
+			}
+			if (write(fd_log,"\n",sizeof(char)) < 0) {
+				perror("log write endline");
+				running = 0;
+				break;
+			}
 		}
 
 		if (FD_ISSET(FD_STDIN,&fds)) {
-/*			if (read(FD_STDIN,buffer,sizeof(char)) < 0) {
-				perror("read");
-				break;
-			}
-			if (write(fd_master,buffer,sizeof(char)) < 0) {
-				perror("write");
-				break;
-			}*/
 			if (read(FD_STDIN,&c,sizeof(char)) < 0) {
 				perror("read");
+				running = 0;
+				break;
+			}
+			if (c == 'Q') {   // TODO catch ^c signal
+				running = 0;
+//				write(fd_master,"#quit\nynnn",10);
 				break;
 			}
 			if (write(fd_master,&c,sizeof(char)) < 0) {
 				perror("write");
+				running = 0;
 				break;
 			}
 		}
 
 	}
 	
-	wait();
+//	wait();
 	close(fd_master);
+	close(fd_log);
 	return 0;
 }
 
